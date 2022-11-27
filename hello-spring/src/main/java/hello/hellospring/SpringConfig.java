4831209a4ca3ec8bba76e73b73ffb090ea292547
jerry3269 <jerry3269@ajou.ac.kr>
2022-11-27 22:41:43 +0900

    first upload

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,42 @@
+package hello.hellospring;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+    private final DataSource dataSource;
+    private final EntityManager em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository) {
+        this.dataSource = dataSource;
+        this.em = em;
+        this.memberRepository = memberRepository;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//
+//    }
+}
